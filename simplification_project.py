370bd4185ce747880673404e671f5a34b36da4e3
Anne Staff <anne.staff@pm.me>
2022-12-08 15:42:24 +0100

    wrk

--- simplification_project.py
+++ simplification_project.py
@@ -39,19 +39,24 @@ def model(Y, t, pv=210, delta_v=5, km=0.6, beta=5e-7, beta_prim=3e-8, delta_i=2,
 legend = ['V', 'I', 'F', 'Cn', 'E', 'Bn', 'P', 'Am']
 
 
-def plot_model(y0=np.array([V0, 0, 0, 100, 0, 100, 0, 0]), t0=0, interval=6, graph_step=100000):
+def run_model(y0=np.array([V0, 0, 0, 100, 0, 100, 0, 0]), t0=0, interval=6, graph_step=10000):
     tt = np.linspace(t0, interval, graph_step)
 
     y = ddeint(model, lambda t: y0, tt)
-    fig = plt.figure()
-    fig.subplots(nrows=int(len(y0) / 2), ncols=2)
-    for u in range(len(y0)):
-        # plt.subplot(int(u / 2), int(u % 2), u)
-        fig.add_subplot()
-        plt.plot(tt, y[:, u], label=legend[u])
-    plt.legend()
-    plt.show()
     return {"y0": y0, "tt": tt, "y": y}
 
 
-my_model = plot_model()
+def plot_model(y, y0=np.array([V0, 0, 0, 100, 0, 100, 0, 0]), tt=np.linspace(0, 6, 10000)):
+    fig = plt.figure()
+    for u in range(len(y0)):
+        fig.add_subplot(int(u/2 + 1), int(u % 2 + 1), int(u % 2 + 1))
+        plt.plot(tt, y[:, u], label=legend[u])
+    plt.legend()
+    plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+    # fig.set_tight_layout(True)
+    fig.show()
+
+
+my_model = run_model()
+plot_model(my_model["y"])
+print("Done")
